5a519785371c168b2e31544b8725e46d31a1899a
Lars Brinkhoff <lars@nocrew.org>
2018-11-30 08:19:02 +0100

    Update TVREAD to version 90.

--- build/misc.tcl
+++ build/misc.tcl
@@ -1078,7 +1078,7 @@ respond "*" ":midas bkph; ts zap_zap\r"
 expect ":KILL"
 
 # View bitmap file on TV.
-respond "*" ":midas sys2; ts tvread_sysen2; tvread\r"
+respond "*" ":midas sys2; ts tvread_vis; tvread\r"
 expect ":KILL"
 
 # Save TV display as bitmap file.
